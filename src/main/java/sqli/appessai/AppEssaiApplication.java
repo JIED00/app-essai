99a9812b1db2c5cdd5e94c1f5929b5f4d8860af9
Mohammed Jied <mjied@smb.sqli.com>
2024-03-19 13:14:13 +0100

    initial

---
+++ src/main/java/sqli/appessai/AppEssaiApplication.java
@@ -0,0 +1,13 @@
+package sqli.appessai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEssaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEssaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sqli/appessai/AppEssaiApplicationTests.java
@@ -0,0 +1,13 @@
+package sqli.appessai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEssaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
